971ee8e4f68116d5e1ca222410e6b86c4b7ae613
Vishal <kvishal126@gmail.com>
2024-10-20 09:38:46 +0530

    A non dynamic version of etch a sketch

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const container = document.querySelector("#container");
-
-for(let i=0 ;i<256; i++){
+// grid_size =prompt ("Enter grid size (Max 100)");
+for(let i=0 ;i<16**2; i++){
 const grid = document.createElement("div");
 grid.classList.add("innerGrid");
 container.appendChild(grid);
 
 }
-const grids = document.querySelectorAll(".innerGrid");
+const grids = document.querySelectorAll(".innerGrid"); //for selecting all elements with class innergrid
 
 
-
-grids.forEach(grid => {
+                                                              
+grids.forEach(grid => {                                           //for adding event listener to each grid in grids
   grid.addEventListener("mouseover", () => {
-    const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);  //choose a random colour
     grid.style.backgroundColor = randomColor;
   });
 });
 
+// Math.random()*16777215 this is like probabilty function that returns b/w 0 and 16777215
+//you can use Math.floor(Math.random()*num)  and it will return any number betwenn 0 and num 
 
 
 
