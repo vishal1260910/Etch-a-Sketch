176a0543c6f0bd21650e0f2f60e3e771ccbe6a4f
Vishal <kvishal126@gmail.com>
2024-10-20 11:40:05 +0530

    Added dynamic behaviour to the grid

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const container = document.querySelector("#container");
-// grid_size =prompt ("Enter grid size (Max 100)");
-for(let i=0 ;i<16**2; i++){
+ grid_size =prompt ("Enter grid size (Max 100)");
+for(let i=0 ;i<grid_size**2; i++){
 const grid = document.createElement("div");
 grid.classList.add("innerGrid");
 container.appendChild(grid);
 
 }
 const grids = document.querySelectorAll(".innerGrid"); //for selecting all elements with class innergrid
-
+grids.forEach(grid =>{                                                //grids is a nodeList to apply to all nodes you must use forEach loop
+  grid.style.width=(600/grid_size)-2 +"px" ;
+  grid.style.height =(600/grid_size)-2 + "px" ;
+})
 
                                                               
 grids.forEach(grid => {                                           //for adding event listener to each grid in grids
